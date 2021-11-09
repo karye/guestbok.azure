6641a09477bcb846085033477e92f69aab3e45fe
karye <karye.webb@gmail.com>
2021-11-09 17:23:56 +0100

    Start

---
+++ lasa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gästboken</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h1 class="display-4">Gästboken</h1>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="gastbok.php">Skriva</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="lasa.php">Läsa</a>
+            </li>
+        </ul>
+        <?php
+        // Namn på filen som sparas
+        $filnamn = "gastbok.txt";
+
+        // Läs in textfilen
+        $texten = file_get_contents($filnamn);
+
+        // Skriv ut allt
+        echo $texten;
+        ?>
+    </div>
+</body>
+</html>
---
+++ spara.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gästbok</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h1 class="display-4">Gästbok</h1>
+        <?php
+        // Ta emot data som skickas
+        $rubrik = filter_input(INPUT_POST, 'rubrik', FILTER_SANITIZE_STRING);
+        $meddelande = filter_input(INPUT_POST, 'meddelande', FILTER_SANITIZE_STRING);
+        $namn = filter_input(INPUT_POST, 'namn', FILTER_SANITIZE_STRING);
+
+        // Finns data?
+        if ($rubrik && $meddelande && $namn) {
+
+            // Namn på filen som sparas
+            $filnamn = "gastbok.txt";
+
+            // Sätt samman texten
+            $texten = "<h2>$rubrik</h2>\n" .
+                      "<p>$meddelande</p>\n" .
+                      "<p>$namn</p>";
+
+            file_put_contents($filnamn, $texten);
+
+            // Berätta för användaren att texten har sparats
+            echo "<p class=\"alert alert-success\">Din meddelande har sparats!</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
+
+<!-- 
+Varianter
+- Spara en faktura
+- Spara en logg
+- Spara enligt en mall..
+ -->
